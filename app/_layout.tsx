508828b57012a622b844096acfa68c198254e7d7
Lucas <48874219+LucasVlad@users.noreply.github.com>
2025-12-03 23:10:39 -0500

    Setting up colour constants

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack />
+    </ThemeProvider>
+  );
 }
